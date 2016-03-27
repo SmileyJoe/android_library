12530a98d6ec28c9a392e9469c111293399c78ca
SmileyJoe <cody.strugnell@gmail.com>
2016-03-27 19:06:41 +0200

    Adding tests to activity

--- app/src/main/java/com/smileyjoedev/library/activity/DebugActivity.java
+++ app/src/main/java/com/smileyjoedev/library/activity/DebugActivity.java
@@ -9,6 +9,9 @@ import android.widget.TextView;
 import com.smileyjoedev.debug.Debug;
 import com.smileyjoedev.library.R;
 import com.smileyjoedev.library.activity.base.BaseActivity;
+import com.smileyjoedev.library.object.TestDebugObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by cody on 2016/03/27.
@@ -34,10 +37,53 @@ public class DebugActivity extends BaseActivity {
 
         setInformation();
 
-        Debug.d("Test");
+        makeLogs();
     }
 
-    public void setInformation(){
+    private void makeLogs(){
+        int[] intArray = new int[]{0,1,2,3,4,5,6,7,8,9};
+
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("Element 1");
+        stringArrayList.add("Element 2");
+        stringArrayList.add("Element 3");
+        stringArrayList.add("Element 4");
+
+        ArrayList<TestDebugObject> customArrayList = new ArrayList<>();
+        customArrayList.add(new TestDebugObject("Name 2", 5, true));
+        customArrayList.add(new TestDebugObject("Name 3", 10, true));
+        customArrayList.add(new TestDebugObject("Name 4", 7, false));
+        customArrayList.add(new TestDebugObject("Name 5", 28, true));
+        customArrayList.add(new TestDebugObject("Name 6", 54, false));
+
+        Debug.d("----------- Log tests ----------");
+
+        Debug.d("Debug");
+        Debug.i("Information");
+        Debug.w("Warning");
+        Debug.e("Error");
+        Debug.v("Verbose");
+
+        Debug.trace(getBaseContext(), Debug.DEBUG);
+
+        Debug.initMonitor(getBaseContext(), "Monitor test");
+        Debug.monitor("log 1");
+        Debug.endMonitor();
+
+        Debug.d("More", "Then", "One", "String");
+        Debug.d("Handles objects without conversion", 500, 500.99f, 0.98, true);
+
+        Debug.d("Int[]", intArray);
+        Debug.d("ArrayList<String>", stringArrayList);
+
+        Debug.d("Custom object (TestDebugObject)", new TestDebugObject("Name 1", 20, false));
+
+        Debug.d("ArrayList<TestDebugObject>", customArrayList);
+
+        Debug.d("----------- Log tests ----------");
+    }
+
+    private void setInformation(){
         TextView textInformation = (TextView) findViewById(R.id.text_debug_information);
 
         String information = getString(R.string.text_debug_instructions) + System.lineSeparator() + System.lineSeparator();
