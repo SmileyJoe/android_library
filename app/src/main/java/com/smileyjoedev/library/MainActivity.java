0a0dc8c9cf70a1cb8dbcd9148cbece3087354349
SmileyJoe <cody.strugnell@gmail.com>
2016-03-27 13:51:53 +0200

    Nav drawer, Tool bar

--- app/src/main/java/com/smileyjoedev/library/MainActivity.java
+++ app/src/main/java/com/smileyjoedev/library/MainActivity.java
@@ -1,13 +1,28 @@
 package com.smileyjoedev.library;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.smileyjoedev.library.activity.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    public static Intent getIntent(Context context){
+        Intent intent = new Intent(context, MainActivity.class);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public int getNavMenuId() {
+        return R.id.navigation_home;
+    }
+
 }
