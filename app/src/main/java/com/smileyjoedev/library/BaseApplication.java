d2def1798951fd5cce9674bd4cee8d89bd554581
SmileyJoe <cody.strugnell@gmail.com>
2016-03-27 21:40:42 +0200

    Adding base application

---
+++ app/src/main/java/com/smileyjoedev/library/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.smileyjoedev.library;
+
+import android.app.Application;
+import android.util.Log;
+
+/**
+ * Created by cody on 2016/03/27.
+ */
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
